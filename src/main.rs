565c03504c3daf1e80517a999019d4a750fc3907
Sandesh Soni <hello@sandeshsoni.com>
2020-02-10 18:36:37 +0530

    add simple duntion that prints

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ fn main(){
     let y = 6;
     let z = x + y;
     println!("Sum of nos is {}", z);
+
+fn next_birthday(name: &str, age: u8) {
+    let next_age = age + 1;
+    println!("Hi {},On next birthday you will be {}",name, next_age);
+}
+
+next_birthday("Ramesh", 21);
+next_birthday("Umesh", 27);
+
 }
