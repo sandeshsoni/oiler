b2566a997997b5c469f62051db758f414165425e
Sandesh Soni <hello@sandeshsoni.com>
2020-02-10 18:41:55 +0530

    Add a simple function that returns output

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ fn next_birthday(name: &str, age: u8) {
     println!("Hi {},On next birthday you will be {}",name, next_age);
 }
 
+fn square_maker(number: u8) -> u8{number * number}
+
 next_birthday("Ramesh", 21);
 next_birthday("Umesh", 27);
+
+square_maker(1);
+square_maker(11);
+next_birthday("Utkarsh", square_maker(2));
 
 }
